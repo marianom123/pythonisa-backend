914cd038d33f66a7cf9927a9c6c87bf6f8b3496a
marianom123 <marianomartino@msn.com>
2024-07-06 08:25:01 -0300

    Actualización de la APP.PY. Ya funciona el CRUD

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -23,14 +25,17 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql.init_app(app)
 
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS
+
 @app.route('/')
 def index():
   return render_template('productos/index_test.html')
 
-
+# Vista de administración de produtcos. 
 @app.route('/admin')
 def admin():
-  sql = "SELECT * FROM productos"
+  sql = "SELECT * FROM productos ORDER BY id DESC"
   conn = mysql.connection
   cursor = conn.cursor()
   cursor.execute(sql)
@@ -38,6 +43,92 @@ def admin():
   conn.commit()
   
   return render_template('productos/admin.html', productos=productos)
+
+# Vista de Agregar Nuevo prodcuto.
+@app.route('/store', methods=['POST'])
+def create():
+  _titulo = request.form['titulo']
+  _descripcion = request.form['descripcion']
+  _precio = request.form['precio']
+  _foto = request.files['foto']
+  
+  now = datetime.now()
+  time = now.strftime("%Y%m%d_%H%M%S")
+  
+  if _foto.filename != '':
+    fotoname = time + '_' + _foto.filename
+    _foto.save("uploads/"+fotoname)
+    
+  sql = "INSERT INTO `productos` \
+                (`titulo`, `descripcion`, `precio`, `foto`) \
+                VALUES (%s, %s, %s, %s);"
+  datos = (_titulo, _descripcion, _precio, fotoname)
+  
+  conn = mysql.connection
+  cursor = conn.cursor()
+  cursor.execute(sql, datos)
+  conn.commit()
+  
+  return redirect('admin')
+
+# Eliminar un producto
+@app.route('/delete/<int:id>')
+def delete(id):
+  sql = "DELETE FROM productos WHERE id=%s;"
+  conn = mysql.connection
+  cursor = conn.cursor()
+  cursor.execute(sql, (id,))
+  conn.commit()
+  
+  return redirect('/admin')
+
+@app.route('/editar/<int:id>')
+def editar(id):
+  sql = "SELECT * FROM productos WHERE id=%s;"
+  conn = mysql.connection
+  cursor = conn.cursor()
+  cursor.execute(sql, (id,))
+  productos = cursor.fetchall()
+  conn.commit()
+  
+  return render_template('/productos/editar.html', productos = productos)
+
+@app.route('/update', methods = ['POST'])
+def update():
+  _id = request.form['id']
+  _titulo = request.form['titulo']
+  _descripcion = request.form['descripcion']
+  _precio = request.form['precio']
+  _foto = request.files['foto']
+  
+  sql = "UPDATE productos SET titulo=%s, descripcion=%s, precio=%s WHERE id=%s;"
+  datos = (_titulo, _descripcion, _precio, _id)
+  
+  conn = mysql.connection
+  cursor = conn.cursor()
+  cursor.execute(sql, datos)
+  productos = cursor.fetchall()
+  conn.commit()
+  
+  now = datetime.now()
+  time = now.strftime("%Y%m%d_%H%M%S")
+  
+  if _foto.filename != '':
+    fotoname = time + '_' + _foto.filename
+    _foto.save("uploads/"+fotoname)
+    cursor.execute("SELECT foto FROM productos WHERE id=%s", (_id,))
+    fila = cursor.fetchone()
+    if fila and fila[0] is not None:
+      nombreAnterior = fila[0]
+      rutaAnterior = os.path.join(app.config['UPLOADS'], nombreAnterior)
+      if os.path.exists(rutaAnterior):
+        os.remove(rutaAnterior)
+    cursor.execute("UPDATE productos SET foto=%s WHERE id=%s;", (fotoname, _id,))
+    conn.commit()
+    cursor.close()
+
+  
+  return redirect('admin')
 
 if __name__ == '__main__':
   app.run(debug = True)
